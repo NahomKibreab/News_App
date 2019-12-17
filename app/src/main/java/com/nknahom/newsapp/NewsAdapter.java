924906dadc271bf1c45aea75cf46d8fbe7ed4b81
Nahom Kibreab <nknahom@gmail.com>
2019-12-16 20:04:11 -0700

    Date time fixed

--- app/src/main/java/com/nknahom/newsapp/NewsAdapter.java
+++ app/src/main/java/com/nknahom/newsapp/NewsAdapter.java
@@ -14,9 +14,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -45,31 +48,15 @@ public class NewsAdapter extends ArrayAdapter<News> {
 
         // Find the TextView in the news_item.xml layout with the ID webPublicationDate
         TextView webPublicationDate = listItemView.findViewById(R.id.webPublicationDate);
-        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
-        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd-MM-yyy", Locale.ENGLISH);
-        LocalDate date = LocalDate.parse(currentNews.getDate(), inputFormatter);
-        String formattedDate = outputFormatter.format(date);
 
-        LocalDate todayDate = LocalDate.now();
-
-        String check = Integer.toString(date.compareTo(todayDate));
-        Log.v("Today Date", check);
-
-        if (date.compareTo(todayDate) > 0){
-
-            DateTimeFormatter todayFormatter = DateTimeFormatter.ofPattern("h", Locale.ENGLISH);
-
-            LocalTime hours = LocalTime.parse(currentNews.getDate(),inputFormatter);
-            int checkHour = Integer.parseInt(todayFormatter.format(hours));
-            if (checkHour < 1){
-                webPublicationDate.setText("Now");
-            } else {
-                webPublicationDate.setText(hours + " hours ago");
-            }
-        } else {
-            webPublicationDate.setText(formattedDate);
+        SimpleDateFormat myFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        SimpleDateFormat myResult = new SimpleDateFormat("E dd MMM yyyy HH.mm zzz");
+        try {
+            Date myDate = myFormat.parse(currentNews.getDate());
+            webPublicationDate.setText(myResult.format(myDate));
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
-
 
         // Find the TextView in the news_item.xml layout with the ID webTitle
         TextView webTitle = listItemView.findViewById(R.id.webTitle);
